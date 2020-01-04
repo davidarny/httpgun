c601803ed6acb5148072ee178eb8fa7fbf1ec6d8
David Arutiunian <arutunjan666@mail.com>
2020-01-04 19:43:01 +0300

    refactor: change logger field to @Slf4j

--- src/main/java/org/httpgun/HttpGun.java
+++ src/main/java/org/httpgun/HttpGun.java
@@ -1,23 +1,20 @@
 package org.httpgun;
 
 import com.google.common.base.Stopwatch;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.cli.*;
 import org.httpgun.attacker.HttpAttacker;
 import org.httpgun.caller.OkHttpCallerFactory;
 import org.httpgun.config.ConfigProvider;
 import org.httpgun.config.PropertiesFileConfigProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.lang.invoke.MethodHandles;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class HttpGun {
     public static final int EXIT_SUCCESS = 0;
     public static final double MILLISECOND = 1000.0;
-
-    public static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
 
     public static void main(String[] args) {
         val config = new PropertiesFileConfigProvider();
@@ -32,7 +29,7 @@ public class HttpGun {
             val concurrency = options.getConcurrency();
             val total = Stopwatch.createStarted();
 
-            logger.info("\n\n==================== ATTACK {} ====================\n", url);
+            log.info("\n\n==================== ATTACK {} ====================\n", url);
 
             val attacker = new HttpAttacker(options, config, factory);
             val stats = attacker.attack();
@@ -44,30 +41,30 @@ public class HttpGun {
             val average = stats.getTimersAverage();
             val bytes = stats.getTotalBytesCount();
 
-            logger.info("\n\n==================== REPORT ====================\n");
+            log.info("\n\n==================== REPORT ====================\n");
 
-            logger.info("Concurrency Level: {}", concurrency);
-            logger.info("Total time: {}s", StringUtils.friendlyDouble(total.elapsed(TimeUnit.MILLISECONDS) / MILLISECOND));
-            logger.info("Total requests: {}", num);
-            logger.info("Total fails: {}", fails);
-            logger.info("Total RPS: {}", StringUtils.friendlyDouble(sum / concurrency / MILLISECOND));
-            logger.info("Total bytes transmitted: {}", bytes);
+            log.info("Concurrency Level: {}", concurrency);
+            log.info("Total time: {}s", StringUtils.friendlyDouble(total.elapsed(TimeUnit.MILLISECONDS) / MILLISECOND));
+            log.info("Total requests: {}", num);
+            log.info("Total fails: {}", fails);
+            log.info("Total RPS: {}", StringUtils.friendlyDouble(sum / concurrency / MILLISECOND));
+            log.info("Total bytes transmitted: {}", bytes);
             if (average.isPresent()) {
-                logger.info("Average response time: {}ms", StringUtils.friendlyDouble(average.getAsDouble()));
+                log.info("Average response time: {}ms", StringUtils.friendlyDouble(average.getAsDouble()));
             }
-            logger.info("50%: {}ms", stats.getPercentileBy(50));
-            logger.info("80%: {}ms", stats.getPercentileBy(80));
-            logger.info("90%: {}ms", stats.getPercentileBy(90));
-            logger.info("95%: {}ms", stats.getPercentileBy(95));
-            logger.info("99%: {}ms", stats.getPercentileBy(99));
-            logger.info("100%: {}ms", stats.getPercentileBy(100));
+            log.info("50%: {}ms", stats.getPercentileBy(50));
+            log.info("80%: {}ms", stats.getPercentileBy(80));
+            log.info("90%: {}ms", stats.getPercentileBy(90));
+            log.info("95%: {}ms", stats.getPercentileBy(95));
+            log.info("99%: {}ms", stats.getPercentileBy(99));
+            log.info("100%: {}ms", stats.getPercentileBy(100));
 
             System.exit(EXIT_SUCCESS);
         } catch (ParseException e) {
             controller.printHelp();
-            logger.error(config.get("exception_message_template", String.class), e.getMessage());
+            log.error(config.get("exception_message_template", String.class), e.getMessage());
         } catch (InterruptedException e) {
-            logger.error(config.get("exception_message_template", String.class), e.getMessage());
+            log.error(config.get("exception_message_template", String.class), e.getMessage());
             Thread.currentThread().interrupt();
         }
     }
--- src/main/java/org/httpgun/attacker/HttpAttacker.java
+++ src/main/java/org/httpgun/attacker/HttpAttacker.java
@@ -1,25 +1,22 @@
 package org.httpgun.attacker;
 
 import com.google.common.base.Stopwatch;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.httpgun.HttpGunOptions;
 import org.httpgun.caller.HttpCaller;
 import org.httpgun.caller.HttpCallerFactory;
 import org.httpgun.config.ConfigProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.lang.invoke.MethodHandles;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.LongStream;
 
+@Slf4j
 public class HttpAttacker {
-    public static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
-
     private final ExecutorService pool;
     private final long termination;
     private final LongStream range;
@@ -55,19 +52,19 @@ public class HttpAttacker {
                 long elapsed = watch.elapsed(TimeUnit.MILLISECONDS);
 
                 if (!response.isSuccessful()) {
-                    logger.info("Fail #{} in {}ms", index.incrementAndGet(), elapsed);
+                    log.info("Fail #{} in {}ms", index.incrementAndGet(), elapsed);
                     stats.incrementFails();
                     return;
                 }
 
                 stats.addTimer(elapsed);
 
-                logger.info("Success #{} in {}ms", index.incrementAndGet(), elapsed);
+                log.info("Success #{} in {}ms", index.incrementAndGet(), elapsed);
                 stats.incrementSuccesses();
             } catch (IOException e) {
                 watch.stop();
                 stats.incrementFails();
-                logger.error(config.get("exception_message_template", String.class), e.getMessage());
+                log.error(config.get("exception_message_template", String.class), e.getMessage());
             }
         }));
 
